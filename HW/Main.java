34f34ac8f620540fb0c0d69bcfc65d6ee9b683f9
Nikita <kozlov.nu@gmail.com>
2023-04-04 21:28:53 +0300

    add redBlackTree

--- HW/Main.java
+++ HW/Main.java
@@ -4,12 +4,11 @@ package HW;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
         
-        final RedBlackTree tree = new RedBlackTree();
+        final RedBlackTree<Integer> tree = new RedBlackTree<Integer>();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             while (true) {
                 try {
@@ -17,7 +16,6 @@ public class Main {
                     tree.add(value);
                     System.out.println("finish");
                 } catch (Exception ignored) {
-                    // TODO: handle exception
                 }
             }
         } catch (IOException e) {
